eec234a16adb7606306dde1aaed6d3aefe0de2f4
Wang Xing <wangstar1031@hotmail.com>
2019-08-09 00:57:04 +0800

    Communicate with server for saving user id.

--- js/background.js
+++ js/background.js
@@ -3,7 +3,14 @@ chrome.contextMenus.create({
     contexts:["selection"], 
     onclick: function(info, tab) {
         // sendSearch(info.selectionText);
-        alert(info.selectionText);
-        $.get("http://localhost/getPhoneNumbers/?number=" + info.selectionText);
+        // alert(info.selectionText);
+        // $.get("http://localhost/getPhoneNumbers/?number=" + info.selectionText);
+        chrome.storage.sync.get(['USER_ID_dataway'], function(result) {
+        	if( !result.USER_ID_dataway){
+        		alert("You didn't login yet. Please login.")
+        	} else{
+		        $.post("http://app.dataway.nl/", {number: info.selectionText, userId: result.USER_ID_dataway});
+        	}
+        });
     }
 });
--- js/contents.js
+++ js/contents.js
@@ -1,1 +1,18 @@
 console.log("contents");
+// web page script for sending user id to extension.
+
+// var data = { type: "USER_LOGIN", text: user_id };
+// window.postMessage(data, "*");
+
+window.addEventListener("message", function(event) {
+    // We only accept messages from ourselves
+    if (event.source != window)
+        return;
+    
+    if (event.data.type && (event.data.type == "USER_LOGIN")) {
+        // console.log("Content script received message: " + event.data.text);
+        var userId = event.data.text;
+        chrome.storage.sync.set({ USER_ID_dataway: userId });
+        console.log("user id is", userId);
+    }
+});
